1411879c943805bdfae5517bfffe3abedff7518e
Carlos Gonzalez <cagonzalez@imail.ag>
2025-05-26 19:00:45 -0500

    Add gluster plugin

---
+++ plugins/lookup/georeplication.py
@@ -0,0 +1,8 @@
+from glustercli.cli.georep import status
+
+
+def status(primary_volume=None, secondary_host=None,
+                          secondary_volume=None, secondary_user="root")
+
+    return status(primary_volume=primary_volume, secondary_host=secondary_host,
+                secondary_volume=secondary_volume, secondary_user=secondary_user)
