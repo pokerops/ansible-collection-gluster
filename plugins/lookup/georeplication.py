63ab59a3b7f60f2ccfbd0211929bbeb3a8e964e1
Carlos Gonzalez <cagonzalez@imail.ag>
2025-05-26 19:38:49 -0500

    Fix custom plugin

--- plugins/lookup/georeplication.py
+++ plugins/lookup/georeplication.py
@@ -1,8 +1,13 @@
+from ansible.errors import AnsibleError, AnsibleParserError
+from ansible.plugins.lookup import LookupBase
+from ansible.utils.display import Display
 from glustercli.cli.georep import status
 
+display = Display()
 
-def status(primary_volume=None, secondary_host=None,
-                          secondary_volume=None, secondary_user="root")
+class LookupModule(LookupBase):
+    def run(self, primary_volume=None, secondary_host=None,
+            secondary_volume=None, secondary_user="root")
 
-    return status(primary_volume=primary_volume, secondary_host=secondary_host,
-                secondary_volume=secondary_volume, secondary_user=secondary_user)
+        return ([status(primary_volume=primary_volume, secondary_host=secondary_host,
+                secondary_volume=secondary_volume, secondary_user=secondary_user)])
