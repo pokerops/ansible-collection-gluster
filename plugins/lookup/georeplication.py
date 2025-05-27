d7efd680795721b979104fd62c4e4f1123a53df1
Carlos Gonzalez <cagonzalez@imail.ag>
2025-05-26 19:47:43 -0500

    Fix plugin

--- plugins/lookup/georeplication.py
+++ plugins/lookup/georeplication.py
@@ -6,8 +6,7 @@ from glustercli.cli.georep import status
 display = Display()
 
 class LookupModule(LookupBase):
-    def run(self, primary_volume=None, secondary_host=None,
-            secondary_volume=None, secondary_user="root")
+    def run(self, terms, variables=None, **kwargs):
 
         return ([status(primary_volume=primary_volume, secondary_host=secondary_host,
-                secondary_volume=secondary_volume, secondary_user=secondary_user)])
+                secondary_volume=secondary_volume, secondary_user=user)])
