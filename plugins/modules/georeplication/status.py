7ca09066e09c184149cafc5464239da07b93037c
Carlos Gonzalez <cagonzalez@imail.ag>
2025-05-27 12:02:46 -0500

    Add join to module

--- plugins/modules/georeplication/status.py
+++ plugins/modules/georeplication/status.py
@@ -23,7 +23,7 @@ def run_module():
     georep_status = status(primary_volume, secondary_host, secondary_volume, secondary_user)
 
     result = {
-        "status": georep_status
+        "status": georep_status.join()
     }
 
     module.exit_json(changed=False, meta=result)
