f3ea78ccbef9c6e2dd46762ab5e8df98fe6a0eeb
Carlos Gonzalez <cagonzalez@imail.ag>
2025-05-29 17:34:55 -0500

    Refactor georep create module logic

--- plugins/modules/georeplication/create.py
+++ plugins/modules/georeplication/create.py
@@ -1,46 +1,48 @@
 from ansible.module_utils.common.validation import safe_eval
 from ansible.module_utils.basic import AnsibleModule
+from glustercli.cli.utils import GlusterCmdException
 from glustercli.cli.georep import create
 
 def run_module():
 
     module_args = {
-        "volume": {"required": True, "type": "str"},
-        "slave": {"required": True, "type": "str"},
-        "user": {"required": True, "type": "str"},
+        "primary_volume": {"default": None, "type": "str"},
+        "secondary_host": {"default": None, "type": "str"},
+        "secondary_volume": {"default": None, "type": "str"},
+        "secondary_user": {"required": True, "type": "str"},
         "push_pem": {"default": True, "type": "bool"},
         "no_verify": {"default": False, "type": "bool"},
         "ssh_port": {"default": "22", "type": "str"},
         "force": {"default": False, "type": "bool"}
     }
 
-    module = AnsibleModule(argument_spec=module_args)
+    module = AnsibleModule(
+        argument_spec=module_args,
+        supports_check_mode=False
+    )
 
-    primary_volume = module.params["volume"]
+    params = {}
 
-    secondary_volume = module.params["volume"]
-
-    secondary_host = module.params["slave"]
-
-    secondary_user = module.params["user"]
-
-    push_pem = module.params["push_pem"]
-
-    no_verify = module.params["no_verify"]
-
-    ssh_port = module.params["ssh_port"]
-
-    force = module.params["force"]
+    for key, value in module_args.items():
+        params[key] = module.params[key]
 
     try:
-        georep_create = create(primary_volume, secondary_host, secondary_volume, secondary_user,
-                               push_pem, no_verify, force, ssh_port)
+        result = dict(
+            changed=True,
+            msg="Command executed successfully",
+            result=create(**params)
+        )
+        module.exit_json(**result)
 
-    except:
-
-        georep_create = ""
-
-    module.exit_json(changed=False, result=georep_create)
+    except GlusterCmdException as e:
+        rc, out, err = e.args[0]
+        module.fail_json(
+            msg=f"Gluster command failed: {err.strip()}",
+            rc=rc,
+            stdout=out,
+            stderr=err,
+            changed=False
+        )
 
 def main():
     run_module()
