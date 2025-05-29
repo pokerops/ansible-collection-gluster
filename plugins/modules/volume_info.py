0cd5c68c9fcfdd6d336c412476c814ad535ac96d
Carlos Gonzalez <cagonzalez@imail.ag>
2025-05-29 15:07:20 -0500

    Add volume info module

---
+++ plugins/modules/volume_info.py
@@ -0,0 +1,41 @@
+from ansible.module_utils.common.validation import safe_eval
+from ansible.module_utils.basic import AnsibleModule
+from glustercli.cli.utils import GlusterCmdException
+from glustercli.cli.volume import info
+
+def run_module():
+    module_args = {
+        "name": {"default": None, "type": "str"}
+    }
+
+    module = AnsibleModule(
+        argument_spec=module_args,
+        supports_check_mode=False
+    )
+
+    result = dict(
+        changed=False,
+        msg='',
+        results=[],
+    )
+
+    try:
+        result['msg'] = "Command executed successfully"
+        result['results'] = info()
+        module.exit_json(**result)
+
+    except GlusterCmdException as e:
+        rc, out, err = e.args[0]
+        module.fail_json(
+            msg=f"Gluster command failed: {err.strip()}",
+            rc=rc,
+            stdout=out,
+            stderr=err,
+            changed=False
+        )
+
+def main():
+    run_module()
+
+if __name__ == '__main__':
+    main()
