4c320789d1e6bf75130a02e1b64eb8cac0b2db27
Carlos Gonzalez <cagonzalez@imail.ag>
2025-05-29 17:28:28 -0500

    Refactor gluster georep delete module logic

--- plugins/modules/georeplication/delete.py
+++ plugins/modules/georeplication/delete.py
@@ -1,43 +1,54 @@
 from ansible.module_utils.common.validation import safe_eval
 from ansible.module_utils.basic import AnsibleModule
-from glustercli.cli.utils import georep_execute
+from glustercli.cli.utils import georep_execute, GlusterCmdException
 
 def run_module():
 
     module_args = {
-        "volume": {"required": True, "type": "str"},
-        "slave": {"required": True, "type": "str"},
-        "user": {"required": True, "type": "str"},
+        "primary_volume": {"default": None, "type": "str"},
+        "secondary_host": {"default": None, "type": "str"},
+        "secondary_volume": {"default": None, "type": "str"},
+        "secondary_user": {"required": True, "type": "str"},
         "reset_sync_time": {"default": False, "type": "bool"}
     }
 
-    module = AnsibleModule(argument_spec=module_args)
+    module = AnsibleModule(
+        argument_spec=module_args,
+        supports_check_mode=False
+    )
 
-    primary_volume = module.params["volume"]
+    params = {}
 
-    secondary_volume = module.params["volume"]
-
-    secondary_host = module.params["slave"]
-
-    secondary_user = module.params["user"]
-
-    reset_sync_time = module.params["reset_sync_time"]
-
-    cmd = [primary_volume,
-           f"{secondary_user}@{secondary_host}::{secondary_volume}",
-           "delete"]
-
-    if reset_sync_time is not None:
-        cmd += ["reset-sync-time"]
+    params = module.params
 
     try:
-        georep_delete = georep_execute(cmd)
+        secondary_target = f"{params['secondary_user']}@{params['secondary_host']}::{params['secondary_volume']}"
+        cmd = [
+            params['primary_volume'],
+            secondary_target,
+            "delete"
+        ]
 
-    except:
+        if params['reset_sync_time'] is not None:
+            cmd += ["reset-sync-time"]
 
-        georep_delete = ""
+        result = dict(
+            changed=True,
+            msg="Command executed successfully",
+            result=georep_execute(cmd)
+        )
+        module.exit_json(**result)
 
-    module.exit_json(changed=False, result=georep_delete)
+    except GlusterCmdException as e:
+        rc, out, err = e.args[0]
+        module.fail_json(
+            msg=f"Gluster command failed: {err.strip()}",
+            rc=rc,
+            stdout=out,
+            stderr=err,
+            result="",
+            changed=False
+        )
 
 def main():
     run_module()
